29163ad48684c2c6e0dd8009afc3c698f9e288c1
TrHgTung <89836403+TrHgTung@users.noreply.github.com>
2024-05-28 13:21:41 +0700

    use State

--- src/api/Mail.js
+++ src/api/Mail.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
+import config from "../config/config.json"
 
+const {SERVER_API} = config;
+const {API_ENDPOINT} = config;
 export class Mail extends Component {
     constructor(){
         super()
         
+        this.state = {
+            mail: [],
+        }
 
-        this.mailApi = `http://localhost:5225/api/MailAPI`
+        // this.mailApi = `http://localhost:5225/api/MailAPI`
     }
 
-    getMailData = async()=>{
-        const res = await fetch(this.mailApi, {
+    getMailData = async() => {
+        const res = await fetch(`${SERVER_API}${API_ENDPOINT}`, {
             method: "GET",
             headers: {
                 // Authorization: `Bearer: ${token}`,
@@ -18,16 +24,31 @@ export class Mail extends Component {
         })
 
         if(res.ok){
-            const data = await res.json()
-            console.log(data)
+            const mail = await res.json();
+            // console.log(data)
+            this.setState({
+                mail : mail,
+            });
         }
     }
-  render() {
-    this.getMailData()
-    return (
-      <div>Mail</div>
-    )
-  }
+
+    componentDidMount = () => {
+        this.getMailData()
+    }
+
+
+    render() {
+        // this.getMailData()
+        const {mail} = this.state
+        console.log(mail);
+        return (
+            <div>
+                {mail.map((mail) => (
+                    <p key={mail.id}>{mail.email}</p>
+                ))}
+            </div>
+        )
+    }
 }
 
 export default Mail
